cd80e0baa9bdcf93d062c0e8a8158b541cff2bd3
Raghav-rv28 <raghavrudhra28@gmail.com>
2024-06-14 22:18:34 -0400

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
-
+import "./App.css";
+import { useState } from "react";
+import Character from "./character";
 function App() {
-  const [num, setNum] = useState(0);
+  const [characters, setCharacters] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Coding Exercise</h1>
-      </header>
-      <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
-        </div>
-      </section>
+    <div className="App-section">
+      <div className="navbar">
+        <button
+          onClick={() => {
+            setCharacters((prev) => prev + 1);
+          }}
+        >
+          Add New Character
+        </button>
+      </div>
+      <div>
+        {Array.from({ length: characters }, (_, index) => index + 1).map(
+          (val) => {
+            console.log(val);
+            return (
+              <div key={val}>
+                <Character val={val} />
+              </div>
+            );
+          },
+        )}
+      </div>
     </div>
   );
 }
